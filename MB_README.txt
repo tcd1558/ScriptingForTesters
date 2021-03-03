db73defb6a2310cb077ebfd54a622d33a8474157
tcd1558 <tcd1558@yahoo.com>
2021-03-02 16:37:49 -0800

    Added exercise files and updated main.py to cover the first 3 videos of Section 1

---
+++ MB_README.txt
@@ -0,0 +1,15 @@
+
+Authorization Tokens
+
+GitHub
+Click the tirangle next to your icon on the right, in the top, black bar. 
+https://github.com/settings/profile
+In the menue on the left select 'Developer Settings'
+https://github.com/settings/apps
+In the menu on the left, select 'Personal access tokens'
+[Generate new toen]
+Note: ScriptingForTesters
+[v] users
+[Generate token]
+Click on the Clip Board to copy the token
+
--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from pip._vendor import requests
 
 
 def print_hi(name):
@@ -12,5 +13,67 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+    print()
 
+    url = "http://jsonplaceholder.typicode.com/photos"
+
+    # get
+    print("01_01: get")
+    response = requests.get(url)
+    print(response.json())
+    print()
+
+    # post
+    print("01_01: post")
+    jsonPayload = {'albumId': 1, 'title': 'test', 'url': 'nothing.com', 'thumbnailUrl': 'nothing.com'}
+    response = requests.post(url,json=jsonPayload)
+    print(response.json())
+    print()
+
+    # put
+    print("01_01: put")
+    url = "http://jsonplaceholder.typicode.com/photos/100"
+    response = requests.put(url, json=jsonPayload)
+    print(response.json())
+    print()
+
+    # delete
+    print("01_01: delete")
+    # url = "http://jsonplaceholder.typicode.com/photos/100"
+    response = requests.delete(url)
+    print(response.json())
+    print()
+
+    # with authentication
+    print("01_02: authentication without credentials")
+    url = 'https://api.github.com/user'
+    response = requests.get(url)
+    print(response.json())
+    print()
+
+    # curl -i -u your_username https://api.github.com/user
+
+    print("01_02:authentication with credentials")
+    # response = requests.get(url, auth=('djw-test', 'password1'))
+    your_username='djw-test@gmail.com'
+    username='djw-test'
+    password='password1'
+    response = requests.get(url, auth=(username, password))  # does not work 20210301
+    print(response.json())
+    print()
+
+    token='abc' # retrieve from token.GitHub.NotebookPWD
+
+    print("01_02: authorization with token")
+    response = requests.get(url,headers={'Authorization': 'Bearer ' + token})
+    print(response.json())
+    print()
+
+    print("01_03: Analyzing the data: Parsing data")
+    response = requests.get(url, headers={'Authorization': 'Bearer ' + token})
+    my_json = response.json()
+    for key in my_json.keys():
+        print (key, my_json[key])
+
+    print()
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
