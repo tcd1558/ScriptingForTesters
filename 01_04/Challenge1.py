29075d3ecf100d8fb567a3fdf94e7e5f8a9abd11
tcd1558 <tcd1558@yahoo.com>
2021-03-12 10:11:05 -0800

    Issue #1 - Add comments to differentiate between url_set.add() and set(url_list).

--- 01_04/Challenge1.py
+++ 01_04/Challenge1.py
@@ -14,14 +14,17 @@ if __name__ == '__main__':
     json_data = response.json()
 
     #create a list for storing the url of each photo
-    url_list = [] # Is a list of dictionaries.
-    url_set = set()
+    url_list = []    # Is a list of dictionaries.
+    url_set = set()  # url_set is an empty set.
     for photo in json_data:
         #add the url for each photo to the url_list
-        url_list.append(photo['url'])
+        url_list.append(photo['url'])   # Do not add the entire list entry photo, but only the key: 'url'
+                                        # and its value. Now the url_list is a list of urls. To extend a list,
+                                        # you use the method append.
         print(photo)
         print(photo['url'])
-        url_set.add(photo['url'])
+        url_set.add(photo['url'])       # To add an element to a set, you use the method add. A set only has
+                                        # unique values.
     #x = input("Proceed..")
     print()
 
@@ -35,5 +38,7 @@ if __name__ == '__main__':
     print("length of url_set: ", len(url_set))
     x = input("Proceed..")
     print()
-    print("Length of set of url_list: ", len(set(url_list)))
+    print("Length of set of url_list: ", len(set(url_list)))    # you can also create the set outside the loop,
+                                                                # by converting the list into a set with the set()
+                                                                # function.
 
