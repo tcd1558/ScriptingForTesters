a395dcaaf7aa81e52e06ec9fb916d6b209a38087
tcd1558 <tcd1558@yahoo.com>
2021-03-11 18:06:29 -0800

    Issue #1 - Solution for Challenge1.py

--- 01_04/Challenge1.py
+++ 01_04/Challenge1.py
@@ -4,22 +4,36 @@
 
 import requests
 
-url = 'https://jsonplaceholder.typicode.com/photos'
+if __name__ == '__main__':
+    url = 'https://jsonplaceholder.typicode.com/photos'
 
-#get the data about the photos
-response = 
+    #get the data about the photos
+    response = requests.get(url)
 
-#read that data into a variable  
-json_data = 
+    #read that data into a variable
+    json_data = response.json()
 
-#create a list for storing the url of each photo
-url_list = []
-for photo in json_data:
-    #add the url for each photo to the url_list
+    #create a list for storing the url of each photo
+    url_list = [] # Is a list of dictionaries.
+    url_set = set()
+    for photo in json_data:
+        #add the url for each photo to the url_list
+        url_list.append(photo['url'])
+        print(photo)
+        print(photo['url'])
+        url_set.add(photo['url'])
+    #x = input("Proceed..")
+    print()
 
-#How many items are in the url list (Should be 5000 since we have 5000 photos in our dataset)?
+    #How many items are in the url list (Should be 5000 since we have 5000 photos in our dataset)?
+    print(url_list)
+    print(len(url_list))
+    x = input("Proceed..")
 
-
-#How many items are there if we turn that list into a set?
-
+    #How many items are there if we turn that list into a set?
+    print(url_set)
+    print("length of url_set: ", len(url_set))
+    x = input("Proceed..")
+    print()
+    print("Length of set of url_list: ", len(set(url_list)))
 
